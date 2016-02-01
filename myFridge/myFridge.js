5d156f0fb9459248cbceefa418ec04647235194c
sdemecilio <stacey.demecilio@live.com>
2016-01-31 18:01:07 -0800

    First change for assignment.

--- myFridge/myFridge.js
+++ myFridge/myFridge.js
@@ -1,23 +1,89 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Products = new Mongo.Collection('products');
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+if (Meteor.isClient)
+{
+	Template.fridge.helpers
+	({
+		products: function()
+		{
+			return Products.find ({
+				place: 'fridge'
+			});
+		}
+	});
+	
+	Template.productList.helpers
+	({
+		products: function()
+		{
+			return Products.find ({
+				place: 'supermarket'
+			});
+		}
+	});
+	
+	Template.fridge.onRendered(function()
+	{
+		var templateInstance = this;
+		
+		templateInstance.$('#fridge').droppable({
+			drop: function(evt, ui)
+			{
+				// create query 
+				var query = {_id: ui.draggable.data('id')};
+				
+				// assign data to query 
+				var changes = {$set: {place: 'fridge'}};
+				
+				// execute query 
+				Products.update(query, changes);
+			}
+		});
+	});
+	
+	Template.productList.onRendered(function()
+	{
+		var templateInstance = this;
+		
+		templateInstance.$('.draggble').draggable({
+			cursor: 'move',
+			helper: 'clone'
+		});
+	});
+	
+	Template.productList.onRendered(function()
+	{
+		var templateInstance = this;
+		
+		templateInstance.$('#supermarket').droppable({
+			drop: function(evt, ui)
+			{
+				var query = {_id: ui.draggable.data('id')};
+				var changes = {$set: {place: 'supermarket'}};
+				Products.update(query, changes);
+			}
+		})
+	})
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+if (Meteor.isServer)
+{
+	Meteor.startup(function (){
+		Products.remove({});
+		
+		// fill database with some products
+		Products.insert
+		({
+			name: 'Milk',
+			img: '/milk.png',
+			place: 'fridge'
+		});
+		
+		Produts.insert
+		({
+			name: 'Bread',
+			img: 'bread.png',
+			place: 'fridge'
+		});
+	});
 }
